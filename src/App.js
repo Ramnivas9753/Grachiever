2294475d564e336fe982ac14d965bc62b236dc64
Lalit Narware <lalitnarware2001@gmail.com>
2023-04-28 16:52:33 +0530

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Blog from './Components/Blog';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    // This Code is bydefault from react app dont touch this code...
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+
+    <>
+      <Navbar />
+      {/* <Home /> */}
+      <Blog />
+      <About />
+      <Contact />
+      <Login />
+      <p className='mt-10' ></p>
+      <Signup />
+      <Footer />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,30 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import Home from './Home';
-import Blog from './Blog';
-import About from './About';
-import Contact from './Contact';
-import Login from './Login';
-import Signup from './Signup';
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <Navbar />
-    {/* <Home /> */}
-    <Blog />
-    <About />
-    <Contact />
-    <Login />
-    <p className='mt-10' ></p>
-    <Signup />
-    <Footer />
-  </>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
