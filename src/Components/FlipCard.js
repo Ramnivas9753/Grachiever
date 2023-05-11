969a1d5f01224ede90e1e1cf8361d2243d675e8f
ramnivasrajput608@gmail.com <ramnivasrajput608@gmail.com>
2023-05-11 19:38:45 +0530

    BtnDone

--- src/Components/FlipCard.js
+++ src/Components/FlipCard.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FaStar } from "react-icons/fa";
+import { FaGreaterThan } from "react-icons/fa";
+import { FaLessThan } from "react-icons/fa";
+
 
 const FlipCard = () => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3'>
       <div className="group relative w-full md:w-64 h-72 bg-red-100  rounded-2xl ">
       <div className="absolute inset-0 bg-white  p-2 rounded-2xl z-10 ">
-            <p className="text-gray-700 ml-3 font-bold flex items-center ">Tools </p>
+            <p className="text-gray-700 ml-3 font-bold  items-center ">Tools<span className='float-right text-x m-2 border-2 border-black rounded-full p-1'><FaGreaterThan /></span></p>
             <p className="text-gray-700 ml-3">HOW IT WORKS?</p>
             <img src={'https://www.classcentral.com/report/wp-content/uploads/2023/04/Pandas-BCG-Banner.png'} className='w-full h-52 p-2 rounded-2xl' />
       </div>
-      <div className='w-52 h-[270px]  overflow-hidden rounded-2xl m-3 p-2 bg-gray-300 transform transition duration-500 group-hover:translate-x-60'>
-      <p className=" ml-3 font-bold flex items-center ">Tools </p>
+      <div className='w-52 h-[270px]  overflow-hidden rounded-2xl m-3 p-2 bg-gray-300 transform transition duration-500 group-hover:translate-x-60'><span className='float-right text-x m-2 border-2 border-black rounded-full p-1'><FaLessThan /></span>
+      <p className=" ml-3 font-bold flex items-center ">Tools</p>
       <p className="ml-3">
       Lorem  Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem 
       </p>
@@ -20,11 +23,11 @@ const FlipCard = () => {
     
     <div className="group relative w-full md:w-64 h-72 bg-red-100  rounded-2xl ">
       <div className="absolute inset-0 bg-white  p-2 rounded-2xl z-10 ">
-            <p className="text-gray-700 ml-3 font-bold flex items-center ">Tools </p>
+            <p className="text-gray-700 ml-3 font-bold  items-center ">Tools <span className='float-right text-x m-2 border-2 border-black rounded-full p-1'><FaGreaterThan /></span></p>
             <p className="text-gray-700 ml-3">HOW IT WORKS?</p>
             <img src={'https://www.classcentral.com/report/wp-content/uploads/2023/04/Pandas-BCG-Banner.png'} className='w-full h-52 p-2 rounded-2xl' />
       </div>
-      <div className='w-52 h-[270px]  overflow-hidden rounded-2xl m-3 p-2 bg-gray-300 transform transition duration-500 group-hover:translate-x-60'>
+      <div className='w-52 h-[270px]  overflow-hidden rounded-2xl m-3 p-2 bg-gray-300 transform transition duration-500 group-hover:translate-x-60'><span className='float-right text-x m-2 border-2 border-black rounded-full p-1'><FaLessThan /></span>
       <p className=" ml-3 font-bold flex items-center ">Tools </p>
       <p className="ml-3">
       Lorem  Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem 
@@ -34,11 +37,11 @@ const FlipCard = () => {
 
     <div className="group relative w-full md:w-64 h-72 bg-red-100  rounded-2xl ">
       <div className="absolute inset-0 bg-white  p-2 rounded-2xl z-10 ">
-            <p className="text-gray-700 ml-3 font-bold flex items-center ">Tools </p>
+            <p className="text-gray-700 ml-3 font-bold  items-center ">Tools <span className='float-right text-x m-2 border-2 border-black rounded-full p-1'><FaGreaterThan /></span></p>
             <p className="text-gray-700 ml-3">HOW IT WORKS?</p>
             <img src={'https://www.classcentral.com/report/wp-content/uploads/2023/04/Pandas-BCG-Banner.png'} className='w-full h-52 p-2 rounded-2xl' />
       </div>
-      <div className='w-52 h-[270px]  overflow-hidden rounded-2xl m-3 p-2 bg-gray-300 transform transition duration-500 group-hover:translate-x-60'>
+      <div className='w-52 h-[270px]  overflow-hidden rounded-2xl m-3 p-2 bg-gray-300 transform transition duration-500 group-hover:translate-x-60'><span className='float-right text-x m-2 border-2 border-black rounded-full p-1'><FaLessThan /></span>
       <p className=" ml-3 font-bold flex items-center ">Tools </p>
       <p className="ml-3">
       Lorem  Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -145,6 +145,9 @@ function Home() {
             <h1 className=" mt-5 text-2xl sm:text-2xl md:text-3xl lg:text-5xl font-bold"><span class="text-[#4eb0e1]">Track</span> The Trending<br/> <span class="text-[#4eb0e1]">Skills</span> Over Time</h1>
             <p className="mt-5  text-lg">Our Skill Graph feature helps you stay up-to-date with the latest skills in demand. our Skill Graph displays the trending skills over time.</p>
             <p  className="mt-5 text-lg">allowing you to identify which skills are gaining popularity and which are losing relevance. With this powerful tool, you can make informed decisions about the skills you want to learn or develop, ensuring you stay ahead of the curve in your chosen field.</p>
+            <div className=' mt-10'>
+       <button className="bg-[#00c5ff] hover:bg-white text-white hover:text-black border-2 border-[#00c5ff] p-2 rounded-xl   px-8 text-lg  font-bold">Learn More</button>
+       </div>
         </div>
     {/* <!--Graph Section--> */}
         <div className="mt-5 md:mt-0">
