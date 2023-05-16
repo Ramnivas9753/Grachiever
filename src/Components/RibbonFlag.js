4b3c1f108bb54438adf603a125f6340a1930ed80
Deepesh Singh Tomar <85624466+yoursdeepu@users.noreply.github.com>
2023-05-16 09:24:17 +0530

    card2

--- src/Components/RibbonFlag.js
+++ src/Components/RibbonFlag.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { HiCurrencyRupee } from "react-icons/hi";
 import "./RibbonFlag.scss";
 
 export default function RibbonFlag() {
-  return <div className="flag"> &#8377; PAID</div>;
+  return <div className="flag flex justify-center gap-2"> <HiCurrencyRupee className="text-xl" />  PAID</div>;
 }
+// &#8377; 
