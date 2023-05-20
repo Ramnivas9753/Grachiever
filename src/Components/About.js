f738b381de82539c1929af7a7e79039dd582b21e
Deepesh Singh Tomar <85624466+yoursdeepu@users.noreply.github.com>
2023-05-20 22:19:43 +0530

    abt

--- src/Components/About.js
+++ src/Components/About.js
@@ -24,7 +24,7 @@ function About() {
       <div className="m-5 md:hidden">
         <img src={image1} alt="" />
       </div>
-      <div className=" grid-cols-1 md:grid-cols-3 lg:grid-cols-3 w-auto md:h-full grid grid-rows-3 grid-flow-col gap-7 mt-10 md:ml-10">
+      <div className=" grid-cols-1 md:grid-cols-3 lg:grid-cols-3 w-auto md:h-full grid grid-rows-1 grid-flow-col gap-7 mt-10 md:ml-10">
         <div className=" h-auto row-span-3 ... z-20">
           <img src={image1} alt="" />
         </div>
