53d309836a88e6edfa76cbfb1e35595d377ede60
Lalit Narware <lalitnarware2001@gmail.com>
2023-05-25 22:31:37 +0530

    changes on navbar, home width, z-index image

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -22,7 +22,7 @@ function Navbar() {
                           
                       <div className="md:hidden ">
                           <button
-                              className="p-2 text-white rounded-md outline-none focus:border-gray-400 focus:border"
+                              className="p-2 rounded-md outline-none focus:border-gray-400 focus:border"
                               onClick={() => setNavbar(!navbar)}
                           >
                               {navbar ? (
@@ -117,52 +117,6 @@ function Navbar() {
                                 </li><hr/>
                                 <li className="rounded-sm relative px-1 py-1 hover:bg-gray-100">
                                 <button className="w-full text-left flex items-center outline-none focus:outline-none">
-                                    <span className="pr-1 flex-1">Skill Graph</span>
-                                    <span className="mr-auto">
-                                    <svg className="fill-current h-4 w-4
-                                        transition duration-150 ease-in-out"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20" >
-                                        <path
-                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
-                                    </svg>
-                                    </span>
-                                </button>
-                                <ul className="bg-white border rounded-sm absolute top-0 right-0 
-                                    transition duration-150 ease-in-out origin-top-left
-                                    min-w-32">
-                                    <li className="px-3 py-1 hover:bg-gray-100">
-                                    <Link to=''>Javascript</Link></li><hr/>
-                                    <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
-                                    <button className="w-full text-left flex items-center outline-none focus:outline-none">
-                                        <span className="pr-1 flex-1">Python</span>
-                                        <span className="mr-auto">
-                                        <svg className="fill-current h-4 w-4
-                                                transition duration-150 ease-in-out"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20">
-                                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
-                                        </svg>
-                                        </span>
-                                    </button>
-                                    <ul className="bg-white border rounded-sm absolute top-0 right-0 
-                                        transition duration-150 ease-in-out origin-top-left
-                                        min-w-32">
-                                        <li className="px-3 py-1 hover:bg-gray-100">
-                                        <Link to=''>Python 2.7</Link></li><hr/>
-                                        <li className="px-3 py-1 hover:bg-gray-100">
-                                        <Link to=''>Python 3+</Link></li><hr/>
-                                    </ul>
-                                    </li><hr/>
-                                    <li className="px-3 py-1 hover:bg-gray-100">
-                                    <Link to=''>Go</Link></li><hr/>
-                                    <li className="px-3 py-1 hover:bg-gray-100">
-                                    <Link to=''>Rust</Link></li><hr/>
-                                </ul>
-                                </li><hr/>
-
-                                <li className="rounded-sm relative px-1 py-1 hover:bg-gray-100">
-                                <button className="w-full text-left flex items-center outline-none focus:outline-none">
                                     <span className="pr-1 flex-1">Tool & Services</span>
                                     <span className="mr-auto">
                                     <svg className="fill-current h-4 w-4
@@ -232,8 +186,11 @@ function Navbar() {
                                         <Link to="/skillConsultancy" className="pr-1 flex-1">Skill Consultancy</Link>
                                     </button>
                                 </li>
-
-
+                                <li className="rounded-sm relative px-1 py-1 hover:bg-gray-100">
+                                    <button className="w-full text-left flex items-center outline-none focus:outline-none">
+                                        <Link to="/skillConsultancy" className="pr-1 flex-1">Skill Graph</Link>
+                                    </button>
+                                </li><hr/>
 
 
                             </ul>
